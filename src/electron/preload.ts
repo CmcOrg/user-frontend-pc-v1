17fdcd1eee27ebf403e68510d38e322fd2749ec7
caoguiming <1677664380@qq.com>
2022-10-25 12:32:29 +0800

    【i】【CmcOrg】

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -6,22 +6,17 @@ window.addEventListener("DOMContentLoaded", () => {
 // 渲染进程（html）给主进程发送消息
 contextBridge.exposeInMainWorld('ipcRenderer', {
     // 需要使用：ipcRenderer.on(channel, listener)，才可以拿到返回值
-    send(channel: TElectronChannel, data?: any) {
+    send(channel: string, data?: any) {
         ipcRenderer.send(channel, data)
     },
     // 使用：.then，即可拿到返回值
-    invoke(channel: TElectronChannel, data?: any) {
+    invoke(channel: string, data?: any) {
         return ipcRenderer.invoke(channel, data)
     },
-    on(channel: TElectronChannel, listener: (event: IpcRendererEvent, data?: any) => void) {
+    on(channel: string, listener: (event: IpcRendererEvent, data?: any) => void) {
         ipcRenderer.on(channel, listener)
     },
+    removeAllListeners(channel: string) {
+        ipcRenderer.removeAllListeners(channel)
+    }
 })
-
-// electron 通道 type
-type TElectronChannel =
-    'electron:minimize'
-    | 'electron:unmaximize'
-    | 'electron:isMaximized'
-    | 'electron:maximize'
-    | 'electron:close' | 'socket:connect'
